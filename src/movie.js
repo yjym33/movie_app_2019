e955e7fb89c31f15a83296964673ee7c4acf42b6
yjym33 <yjym33@naver.com>
2021-02-05 17:37:07 +0900

    css스타일 추가 및 수정

--- src/movie.js
+++ src/movie.js
@@ -9,7 +9,7 @@ function Movie({id, year, title, summary, poster, genres}) {
         <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
             {genres.map((genre, index) => (
                 <li key = {index} className="genres_genre">
                     {index}
@@ -17,7 +17,7 @@ function Movie({id, year, title, summary, poster, genres}) {
                     </li>
         ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 140)}...</p>
     </div>
 </div>
     );
