f8b3a6c76a3d8bcd23c87c6a1240b580a5e05f87
yjym33 <yjym33@naver.com>
2021-02-06 22:20:40 +0900

    today second commit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,21 @@
-import react from 'react';
+import React from 'react';
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({id, year, title, summary, poster, genres}) {
     return (
-    <div class="movie">
+        <Link to={{
+           pathname:'/movie-detail',
+           state: {
+               year,
+               title,
+               summary,
+               poster,
+               genres
+           } 
+        }}>
+        <div class="movie">
         <img src={poster} alt={title} title={title} /> 
         <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
@@ -20,6 +31,7 @@ function Movie({id, year, title, summary, poster, genres}) {
         <p className="movie__summary">{summary.slice(0, 180)}...</p>
     </div>
 </div>
+</Link>
     );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,13 @@ function Navigation() {
   return (
     <div className="nav">
       <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to={{
+        pathname:"/about",
+        state: {
+          fromNavigation : true
+
+        }
+      }}>About</Link>
     </div>
   );
 }
