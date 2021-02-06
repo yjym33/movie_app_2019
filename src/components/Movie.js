6b2cff31d694ac9a05054a9310bb84dbf7f65de7
yjym33 <yjym33@naver.com>
2021-02-06 13:19:47 +0900

    Router commit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -17,7 +17,7 @@ function Movie({id, year, title, summary, poster, genres}) {
                     </li>
         ))}
         </ul>
-        <p className="movie__summary">{summary.slice(0, 140)}...</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
     </div>
 </div>
     );
